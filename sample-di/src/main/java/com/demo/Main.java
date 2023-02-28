6065c43604144fccfef9e36f4fae6271b58056cb
H_Myat_Win <htoomyatwin720@gmail.com>
2023-02-28 10:16:28 +0630

    spring-core base

---
+++ sample-di/src/main/java/com/demo/Main.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import com.demo.beans.Person;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        context.registerShutdownHook();
+
+        Person person=context.getBean(Person.class);
+        person.setName("Hla Hla");
+        System.out.println("Name :" + person.getName());
+    }
+}
